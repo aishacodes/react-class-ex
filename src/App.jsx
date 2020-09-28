9d00ec65b627c1f1e554c12a39cf5451eed44005
Aishat Abdulfatah <aishatabdulfatah81@gmail.com>
2020-09-28 16:09:09 +0100

    merging objects into an array

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,23 @@ import './App.css';
 function App() {
     const course = 'Half Stack application development'
 
-    const part1 = {
-      name: 'Fundamentals of React', 
-      exercises: 10
-    }
-    const part2 ={
-        name:'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-      name: 'State of a component',
-      exercises: 14
-    }
-
+    const parts = [
+      {
+        name: 'Fundamentals of React', 
+        exercises: 10
+      },
+      {
+          name:'Using props to pass data',
+          exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
     return (
       <div>
-        <Header courses = { course } />
-        <Content />
-        <Total ex1={exercises1} ex2 = {exercises2} ex3 = {exercises3} />
-        
+       
       </div>
     )
 }
